a588bf673611611d89fc04a46f575a9d9c3e505c
João Marques <joaohrik@gmail.com>
2020-01-17 15:48:10 +0000

    Added jQuery function

--- inc/classes/DB.php
+++ inc/classes/DB.php
@@ -10,7 +10,6 @@ class DB {
 
     private function __construct() {
         try {
-
             self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=3306;dbname=login_course', 'root', '' );
             self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
             self::$con->setAttribute( PDO::ATTR_PERSISTENT, false );
@@ -18,7 +17,6 @@ class DB {
         } catch (PDOException $e) {
             echo "Could not connect to database."; exit;
         }
-
     }
 
     public static function getConnection() {
